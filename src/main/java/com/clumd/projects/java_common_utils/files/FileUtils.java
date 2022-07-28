3d7f53748b09256d2bf8973aa9f016b0f56d1d10
CalumD <github@clumd.com>
2022-07-28 23:11:41 +0100

    Add some static FileUtils and some unit tests for them

--- src/main/java/com/clumd/projects/java_common_utils/files/FileUtils.java
+++ src/main/java/com/clumd/projects/java_common_utils/files/FileUtils.java
@@ -1,4 +1,215 @@
 package com.clumd.projects.java_common_utils.files;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
 public class FileUtils {
+
+    private FileUtils() {
+    }
+
+    /**
+     * This method takes a file path, and concats all the content into a single string.
+     *
+     * @param file The file path for the file to turn into a string
+     * @return The file as a single string.
+     * @throws IOException Thrown if it could not find, or you don't have permissions for that file.
+     */
+    public static String getFileAsString(final String file) throws IOException {
+        StringBuilder ret = new StringBuilder();
+
+        for (String s : getFileAsStrings(file)) {
+            ret.append(s);
+        }
+
+        return ret.toString();
+    }
+
+    /**
+     * This method takes a file path, and returns an array of strings for each line in the file.
+     *
+     * @param file The file path for the file to turn into string array
+     * @return An element for each line in the file.
+     * @throws IOException Thrown if it could not find, or you don't have permissions for that file.
+     */
+    public static List<String> getFileAsStrings(final String file) throws IOException {
+        checkIfExistsOrIsFolder(file);
+        ArrayList<String> ret = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while (reader.ready()) {
+                ret.add(reader.readLine() + System.lineSeparator());
+            }
+        }
+
+        return ret;
+    }
+
+    /**
+     * This method checks a given input path to ensure it exists and is a file.
+     *
+     * @param path The path to check.
+     * @throws FileNotFoundException Thrown if the path has no file at it, or the path ends at an existing directory.
+     */
+    private static void checkIfExistsOrIsFolder(final String path) throws FileNotFoundException {
+        File file = new File(path);
+
+        if (!file.exists()) {
+            throw new FileNotFoundException(path + " (File not Found)");
+        }
+
+        if (file.isDirectory()) {
+            throw new FileNotFoundException(path + " (Is a directory, not a file)");
+        }
+    }
+
+    /**
+     * This is used to return the raw bytes of a given file.
+     *
+     * @param path The full path to the file to get the bytes from.
+     * @return The byte[] of the entire file contents.
+     * @throws IOException Thrown if there was a problem accessing the requested file.
+     */
+    public static byte[] getFileAsBytes(final String path) throws IOException {
+        if (path == null) {
+            return new byte[]{};
+        }
+
+        checkIfExistsOrIsFolder(path);
+
+        return Files.readAllBytes(new File(path).toPath());
+    }
+
+    /**
+     * This is used to get a file part of the source as a string.
+     *
+     * @param resourceName The name of the resource to get as a string
+     * @return The string value of the resource.
+     * @throws IOException Thrown if there was a problem accessing the requested file
+     */
+    public static String getLocalResourceAsString(final String resourceName) throws IOException {
+        StringBuilder ret = new StringBuilder();
+
+        for (String s : getLocalResourceAsStrings(resourceName)) {
+            ret.append(s);
+        }
+
+        return ret.toString();
+    }
+
+    /**
+     * This is used to get the contents of a file line by line.
+     *
+     * @param resourceName The name of the resource to get as a String array.
+     * @return The line by line values of the resource.
+     * @throws IOException Thrown if there was a problem accessing the requested file
+     */
+    public static List<String> getLocalResourceAsStrings(final String resourceName) throws IOException {
+        ArrayList<String> ret = new ArrayList<>();
+
+        try (BufferedReader reader =
+                     new BufferedReader(new InputStreamReader(
+                             Objects.requireNonNull(
+                                     FileUtils.class.getClassLoader()
+                                             .getResourceAsStream(resourceName)
+                             )
+                     ))
+        ) {
+            while (reader.ready()) {
+                ret.add(reader.readLine() + '\n');
+            }
+        } catch (NullPointerException e) {
+            throw new IOException("Unable to read resource from stream, check relative class path hierarchy.");
+        }
+
+        return ret;
+    }
+
+    /**
+     * This is used to validate the existence (as a directory) and permission to read from the given path.
+     *
+     * @param path The path to validate.
+     * @return The String of the resolved path to that directory (removal of . and .. if necessary)
+     * @throws IOException Thrown if there was a problem accessing that path as a directory.
+     */
+    public static String validateIsDirectory(final String path) throws IOException {
+        File activeDir;
+
+        //resolve canonical
+        try {
+            activeDir = new File(path).getCanonicalFile();
+        } catch (IOException e) {
+            throw new IOException("The path provided could not be resolved to a file system location.");
+        }
+
+        //check attributes
+        if (!activeDir.exists()) {
+            throw new IOException("The path provided does not exist.");
+        }
+        if (!activeDir.isDirectory()) {
+            throw new IOException("The path provided is not a directory.");
+        }
+        if (!activeDir.canRead()) {
+            throw new IOException("The directory provided cannot be read from.");
+        }
+
+        //return valid path
+        return activeDir.getPath();
+    }
+
+    /**
+     * Used to write a single string to a file.
+     *
+     * @param data The String to be written to the file.
+     * @param path The path of the file that we would like to write into
+     * @param append If we should add to the end of the file, or overwrite from the beginning.
+     * @throws IOException Thrown if there was a problem writing to the file.
+     */
+    public static void writeStringToFile(final String data, final String path, final boolean append) throws IOException {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(path, append))){
+            writer.write(data);
+        }
+    }
+
+    /**
+     * Used to write multiple strings to a file.
+     *
+     * @param data The collection of Strings to be written to the file.
+     * @param path The path of the file that we would like to write into
+     * @param append If we should add to the end of the file, or overwrite from the beginning.
+     * @throws IOException Thrown if there was a problem writing to the file.
+     */
+    public static void writeStringsToFile(final Collection<String> data, final String path, final boolean append) throws IOException {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(path, append))){
+            for (String s : data) {
+                writer.write(s);
+            }
+        }
+    }
+
+    /**
+     * Used to write raw bytes to a file.
+     *
+     * @param data The data to be written to the file.
+     * @param path The path of the file that we would like to write into
+     * @param append If we should add to the end of the file, or overwrite from the beginning.
+     * @throws IOException Thrown if there was a problem writing to the file.
+     */
+    public static void writeBytesToFile(final byte[] data, final String path, final boolean append) throws IOException {
+        try(FileOutputStream writer = new FileOutputStream(path, append)){
+            writer.write(data);
+        }
+    }
 }
--- src/test/java/com/clumd/projects/java_common_utils/files/FileUtilsTest.java
+++ src/test/java/com/clumd/projects/java_common_utils/files/FileUtilsTest.java
@@ -1,10 +1,277 @@
 package com.clumd.projects.java_common_utils.files;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class FileUtilsTest {
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void get_file_as_string() throws IOException {
+        assertEquals("{" + System.lineSeparator() + "  \"is testing\": true" + System.lineSeparator() + "}" + System.lineSeparator(), FileUtils.getFileAsString("src/test/resources/files/testConfigFile.json"));
+    }
+
+    @Test
+    void get_file_as_strings() throws IOException {
+        ArrayList<String> testConfig = new ArrayList<>(3);
+        testConfig.add("{" + System.lineSeparator());
+        testConfig.add("  \"is testing\": true" + System.lineSeparator());
+        testConfig.add("}" + System.lineSeparator());
+        assertEquals(testConfig, FileUtils.getFileAsStrings("src/test/resources/files/testConfigFile.json"));
+    }
+
+    @Test
+    void get_resource_as_string() throws IOException {
+        final String asString = FileUtils.getLocalResourceAsString("com/clumd/projects/java_common_utils/files/FileUtils.class");
+
+        assertEquals(6240, asString.length(), 0);
+        assertTrue(asString.contains("com/clumd/projects/java_common_utils/files/FileUtils"));
+    }
+
+    @Test
+    void get_resource_as_strings() throws IOException {
+        final List<String> asStrings = FileUtils.getLocalResourceAsStrings("com/clumd/projects/java_common_utils/files/FileUtils.class");
+
+        assertEquals(61, asStrings.size(), 0);
+        assertTrue(asStrings.get(4).contains("com/clumd/projects/java_common_utils/files/FileUtils"));
+    }
+
+    @Test
+    void get_resource_as_strings_not_found() {
+        try {
+            FileUtils.getLocalResourceAsStrings("com/clumd/projects/java_common_utils/files/F");
+            fail("The previous line should have thrown an exception.");
+        } catch (IOException e) {
+            assertEquals("Unable to read resource from stream, check relative class path hierarchy.", e.getMessage());
+        }
+    }
+
+    @Test
+    void test_file_not_found() throws IOException {
+        try {
+            FileUtils.getFileAsString("src/test/resources/files/logs/thisFileIsNotFound.json");
+            fail("The previous line should have thrown an exception.");
+        } catch (FileNotFoundException e) {
+            assertEquals("src/test/resources/files/logs/thisFileIsNotFound.json (File not Found)", e.getMessage());
+        }
+    }
+
+    @Test
+    void test_file_is_directory_1() throws IOException {
+        try {
+            FileUtils.getFileAsString("src/test/resources/files/logs/");
+            fail("The previous line should have thrown an exception.");
+        } catch (FileNotFoundException e) {
+            assertEquals("src/test/resources/files/logs/ (Is a directory, not a file)", e.getMessage());
+        }
+    }
+
+    @Test
+    void test_file_is_directory_2() throws IOException {
+        try {
+            FileUtils.getFileAsString("src/test/resources/files/logs");
+            fail("The previous line should have thrown an exception.");
+        } catch (FileNotFoundException e) {
+            assertEquals("src/test/resources/files/logs (Is a directory, not a file)", e.getMessage());
+        }
+    }
+
+    @Test
+    void test_loading_file_as_bytes() throws IOException {
+        byte[] bytes = FileUtils.getFileAsBytes("src/test/resources/files/testConfigFile.json");
+        assertTrue(new String(bytes).startsWith("{" + System.lineSeparator() + "  \"is testing\": true" + System.lineSeparator() + "}" + System.lineSeparator()));
+    }
+
+    @Test
+    void test_loading_null_file() throws IOException {
+        byte[] bytes = FileUtils.getFileAsBytes(null);
+        assertEquals(0, bytes.length, 0);
+    }
+
+    @Test
+    void test_non_fs_location() {
+        try {
+            FileUtils.validateIsDirectory("\0");
+            fail("The previous line should have thrown an exception.");
+        } catch (IOException e) {
+            assertEquals("The path provided could not be resolved to a file system location.", e.getMessage());
+        }
+    }
+
+    @Test
+    void test_directory_does_not_exist() {
+        try {
+            FileUtils.validateIsDirectory("src/test/resources/files/notExist");
+            fail("The previous line should have thrown an exception.");
+        } catch (IOException e) {
+            assertEquals("The path provided does not exist.", e.getMessage());
+        }
+    }
+
+    @Test
+    void test_directory_is_actually_file() {
+        try {
+            FileUtils.validateIsDirectory("src/test/resources/files/logs/placeholder.txt");
+            fail("The previous line should have thrown an exception.");
+        } catch (IOException e) {
+            assertEquals("The path provided is not a directory.", e.getMessage());
+        }
+    }
+
+//    @Test
+//    void test_read_directory_denied() {
+//        String path = "src/test/resources/files/deniedRead";
+//        new File(path).setReadable(true, false);
+//        new File(path).setWritable(true, false);
+//        new File(path).setExecutable(true, false);
+//        try {
+//            FileUtils.validateIsDirectory(path);
+//            fail("The previous line should have thrown an exception.");
+//        } catch (IOException e) {
+//            assertEquals("The directory provided cannot be read from.", e.getMessage());
+//        }
+//    }
+
+    @Test
+    void test_directory_is_valid() {
+        String path = "src" + File.separator + "test" + File.separator + "resources" + File.separator + "files" + File.separator + "logs";
+        try {
+            String validDirectoryPath = FileUtils.validateIsDirectory(path);
+            assertNotNull(validDirectoryPath);
+            assertTrue(validDirectoryPath.strip().length() > 0);
+            assertTrue(validDirectoryPath.length() > path.length());
+            assertTrue(validDirectoryPath.contains(path));
+        } catch (Exception e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void test_writing_file_creates_if_not_exists() throws IOException {
+        String path = "src/test/resources/files/" + "not_exists.txt";
+        String data = "hello" + System.lineSeparator();
+
+        try {
+            FileUtils.getFileAsString(path);
+            fail("The previous method should have thrown an exception.");
+        } catch (Exception e) {
+            assertTrue(e.getMessage().contains("(File not Found)"));
+        }
+
+        FileUtils.writeStringToFile(data, path, false);
+        assertEquals("hello" + System.lineSeparator(), FileUtils.getFileAsString(path));
+        assertTrue(new File(path).delete());
+
+        try {
+            FileUtils.getFileAsString(path);
+            fail("The previous method should have thrown an exception.");
+        } catch (Exception e) {
+            assertTrue(e.getMessage().contains("(File not Found)"));
+        }
+    }
+
+    @Test
+    void test_writing_string_to_file_overwrite() throws IOException {
+        String path = "src/test/resources/files/" + "test_file_" + UUID.randomUUID() + ".txt";
+        String existingData = "I should disappear";
+        String data = "hello" + System.lineSeparator() + "world" + System.lineSeparator();
+
+        FileUtils.writeStringToFile(existingData, path, false);
+        FileUtils.writeStringToFile(data, path, false);
+        assertEquals(data, FileUtils.getFileAsString(path));
+
+        assertTrue(new File(path).delete());
+    }
+
+    @Test
+    void test_writing_string_to_file_append() throws IOException {
+        String path = "src/test/resources/files/" + "test_file_" + UUID.randomUUID() + ".txt";
+        String data = "hello" + System.lineSeparator() + "world" + System.lineSeparator();
+
+        FileUtils.writeStringToFile(data, path, true);
+        FileUtils.writeStringToFile(data, path, true);
+        assertEquals(data + data, FileUtils.getFileAsString(path));
+
+        assertTrue(new File(path).delete());
+    }
+
+    @Test
+    void test_writing_strings_to_file_overwrite() throws IOException {
+        String path = "src/test/resources/files/" + "test_file_" + UUID.randomUUID() + ".txt";
+        Collection<String> existingData = List.of("I should" + System.lineSeparator(), "disappear" + System.lineSeparator());
+        Collection<String> data = List.of("hello" + System.lineSeparator(), "world" + System.lineSeparator());
+
+        FileUtils.writeStringsToFile(existingData, path, false);
+        FileUtils.writeStringsToFile(data, path, false);
+        assertEquals(data, FileUtils.getFileAsStrings(path));
+
+        assertTrue(new File(path).delete());
+    }
+
+    @Test
+    void test_writing_strings_to_file_append() throws IOException {
+        String path = "src/test/resources/files/" + "test_file_" + UUID.randomUUID() + ".txt";
+        Collection<String> data = List.of("hello" + System.lineSeparator(), "world" + System.lineSeparator());
+
+        FileUtils.writeStringsToFile(data, path, true);
+        FileUtils.writeStringsToFile(data, path, true);
+        Collection<String> actualStrings = FileUtils.getFileAsStrings(path);
+        assertEquals(data.size() * 2, actualStrings.size(), 0);
+        assertTrue(actualStrings.containsAll(data));
+        for (String s : actualStrings) {
+            assertTrue(data.contains(s));
+        }
+
+        assertTrue(new File(path).delete());
+    }
+
+    @Test
+    void test_writing_bytes_to_file_overwrite() throws IOException {
+        String path = "src/test/resources/files/" + "test_file_" + UUID.randomUUID() + ".txt";
+        byte[] existingData = new byte[]{11, 12, 13, 14, 15};
+        byte[] data = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        FileUtils.writeBytesToFile(existingData, path, false);
+        FileUtils.writeBytesToFile(data, path, false);
+
+        byte[] actualData = FileUtils.getFileAsBytes(path);
+        assertEquals(data.length, actualData.length, 0);
+
+        for (int i = 0; i < actualData.length; i++) {
+            assertEquals(data[i], actualData[i]);
+        }
+
+        assertTrue(new File(path).delete());
+    }
+
+    @Test
+    void test_writing_bytes_to_file_append() throws IOException {
+        String path = "src/test/resources/files/" + "test_file_" + UUID.randomUUID() + ".txt";
+        byte[] data = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        byte[] doubleData = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        FileUtils.writeBytesToFile(data, path, true);
+        FileUtils.writeBytesToFile(data, path, true);
+
+        byte[] actualData = FileUtils.getFileAsBytes(path);
+
+        assertEquals(doubleData.length, actualData.length, 0);
+
+        for (int i = 0; i < actualData.length; i++) {
+            assertEquals(doubleData[i], actualData[i]);
+        }
+
+        assertTrue(new File(path).delete());
     }
 }
