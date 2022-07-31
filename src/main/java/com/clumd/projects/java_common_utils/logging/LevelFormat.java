8153fe33795e213bbf838a562fa9e6589be27a2f
CalumD <github@clumd.com>
2022-07-31 22:39:46 +0100

    Update the LevelFormat to remove the default reset modifier now that I have better understanding of ANSI codes. Also added a convenience method to create a format by just passing a collection of all wanted modifiers in one.

--- src/main/java/com/clumd/projects/java_common_utils/logging/LevelFormat.java
+++ src/main/java/com/clumd/projects/java_common_utils/logging/LevelFormat.java
@@ -2,29 +2,38 @@ package com.clumd.projects.java_common_utils.logging;
 
 import lombok.Getter;
 
-public enum LevelFormat {
+import java.io.Serializable;
+import java.util.Collection;
 
+public enum LevelFormat implements Serializable {
+
+    // MODIFIERS
     RESET("\033[0m"),
-    BOLD("\033[0;1m"),
+    BOLD("\033[1m"),
+    UNDERLINE("\033[4m"),
+    ITALIC("\033[3m"),
 
-    BLACK("\033[0;30m"),
-    RED("\033[0;31m"),
-    GREEN("\033[0;32m"),
-    YELLOW("\033[0;33m"),
-    BLUE("\033[0;34m"),
-    PURPLE("\033[0;35m"),
-    CYAN("\033[0;36m"),
-    WHITE("\033[0;37m"),
+    // BASE COLOURS
+    BLACK("\033[30m"),
+    RED("\033[31m"),
+    GREEN("\033[32m"),
+    YELLOW("\033[33m"),
+    BLUE("\033[34m"),
+    PURPLE("\033[35m"),
+    CYAN("\033[36m"),
+    WHITE("\033[37m"),
 
-    BRIGHT_BLACK("\033[0;90m"),
-    BRIGHT_RED("\033[0;91m"),
-    BRIGHT_GREEN("\033[0;92m"),
-    BRIGHT_YELLOW("\033[0;93m"),
-    BRIGHT_BLUE("\033[0;94m"),
-    BRIGHT_PURPLE("\033[0;95m"),
-    BRIGHT_CYAN("\033[0;96m"),
-    BRIGHT_WHITE("\033[0;97m"),
+    // BRIGHT COLOURS
+    BRIGHT_BLACK("\033[90m"),
+    BRIGHT_RED("\033[91m"),
+    BRIGHT_GREEN("\033[92m"),
+    BRIGHT_YELLOW("\033[93m"),
+    BRIGHT_BLUE("\033[94m"),
+    BRIGHT_PURPLE("\033[95m"),
+    BRIGHT_CYAN("\033[96m"),
+    BRIGHT_WHITE("\033[97m"),
 
+    // BASE BACKGROUNDS
     BLACK_BACKGROUND("\033[40m"),
     RED_BACKGROUND("\033[41m"),
     GREEN_BACKGROUND("\033[42m"),
@@ -34,28 +43,42 @@ public enum LevelFormat {
     CYAN_BACKGROUND("\033[46m"),
     WHITE_BACKGROUND("\033[47m"),
 
-    BRIGHT_BLACK_BACKGROUND("\033[0;100m"),
-    BRIGHT_RED_BACKGROUND("\033[0;101m"),
-    BRIGHT_GREEN_BACKGROUND("\033[0;102m"),
-    BRIGHT_YELLOW_BACKGROUND("\033[0;103m"),
-    BRIGHT_BLUE_BACKGROUND("\033[0;104m"),
-    BRIGHT_PURPLE_BACKGROUND("\033[0;105m"),
-    BRIGHT_CYAN_BACKGROUND("\033[0;106m"),
-    BRIGHT_WHITE_BACKGROUND("\033[0;107m"),
-
-    BLACK_UNDERLINE("\033[4;30m"),
-    RED_UNDERLINE("\033[4;31m"),
-    GREEN_UNDERLINE("\033[4;32m"),
-    YELLOW_UNDERLINE("\033[4;33m"),
-    BLUE_UNDERLINE("\033[4;34m"),
-    PURPLE_UNDERLINE("\033[4;35m"),
-    CYAN_UNDERLINE("\033[4;36m"),
-    WHITE_UNDERLINE("\033[4;37m");
+    // BRIGHT BACKGROUNDS
+    BRIGHT_BLACK_BACKGROUND("\033[100m"),
+    BRIGHT_RED_BACKGROUND("\033[101m"),
+    BRIGHT_GREEN_BACKGROUND("\033[102m"),
+    BRIGHT_YELLOW_BACKGROUND("\033[103m"),
+    BRIGHT_BLUE_BACKGROUND("\033[104m"),
+    BRIGHT_PURPLE_BACKGROUND("\033[105m"),
+    BRIGHT_CYAN_BACKGROUND("\033[106m"),
+    BRIGHT_WHITE_BACKGROUND("\033[107m");
 
     @Getter
     private final String formatString;
 
     LevelFormat(final String formatString) {
         this.formatString = formatString;
+    }
+
+    public static String createFormat(Collection<LevelFormat> formatModifiers) {
+        formatModifiers = formatModifiers
+                .stream()
+                .filter(f -> f != RESET)
+                .toList();
+
+        StringBuilder modifiers = new StringBuilder("\033[0;");
+        String formatString;
+
+        for (LevelFormat f : formatModifiers) {
+            modifiers.append(';');
+            formatString = f.getFormatString();
+            formatString = formatString.substring(
+                    formatString.indexOf('[') + 1,
+                    formatString.length() - 1
+            );
+            modifiers.append(formatString);
+        }
+
+        return modifiers.append('m').toString();
     }
 }
