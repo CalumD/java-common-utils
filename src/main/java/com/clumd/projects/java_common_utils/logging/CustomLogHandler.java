f747abde0ae6b66c146690e38cf2f18035914e54
CalumD <github@clumd.com>
2022-08-21 12:33:57 +0100

    Some final touch-ups to a first attempt MVP

--- src/main/java/com/clumd/projects/java_common_utils/logging/CustomLogHandler.java
+++ src/main/java/com/clumd/projects/java_common_utils/logging/CustomLogHandler.java
@@ -2,8 +2,8 @@ package com.clumd.projects.java_common_utils.logging;
 
 import java.util.Map;
 import java.util.UUID;
+import java.util.logging.Level;
 
 public interface CustomLogHandler {
-    void acceptLogRootRefs(UUID specificRunId, String systemId, Map<Integer, String> overriddenThreadNameMappings);
-
+    CustomLogHandler acceptLogRootRefs(UUID specificRunId, Level startingLevel, String systemId, Map<Integer, String> overriddenThreadNameMappings);
 }
