3a67d91292af262f527fb3b222bbce7746d7b683
CalumD <github@clumd.com>
2023-08-30 01:18:56 +0100

    Add a PortableSocket wrapper with option for custom class loader on inputStream init. Still need to add tests and flesh out a couple more implementation details.

---
+++ src/main/java/com/clumd/projects/java_common_utils/base_enhancements/PortableSocket.java
@@ -0,0 +1,198 @@
+package com.clumd.projects.java_common_utils.base_enhancements;
+
+import lombok.Getter;
+import lombok.NonNull;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.*;
+import java.util.Set;
+
+/**
+ * This {@link java.net.Socket} wrapper serves to accumulate some convenience methods for dealing with sockets, such as setting up ObjectStreams,
+ * setting timeouts and preparing customised ClassLoaders for interpreting incoming objects.
+ * <p>
+ * One other extremely useful feature it provides is a PORTABLE method of determining a connection's reachability.
+ * <p>
+ * As per the
+ * <a href="https://download.java.net/java/early_access/jdk21/docs/api/java.base/java/net/InetAddress.html#isReachable(int)">Java documentation</a>
+ * and countless stackoverflow posts, the baked in {@link InetAddress#isReachable(int)} will only make best-effort to perform a regular ICMP PING
+ * request. However, certain operating systems may only allow these calls if the JVM is running with ROOT/ADMIN privileges. This implementation
+ * seeks to improve this, by going a few steps further to determine reachability such as actually trying to construct a Socket to the peer on a
+ * known, provided, port.
+ */
+public class PortableSocket implements AutoCloseable {
+
+    private static final int ONE_SECOND_IN_MS = 1000;
+    private static final int TWO_SECONDS_IN_MS = 2000;
+    private static final int THREE_SECONDS_IN_MS = 3000;
+    private static final int FIVE_SECONDS_IN_MS = 5000;
+    private static final int TEN_SECONDS_IN_MS = 10000;
+    private static final int NO_TIMEOUT = 0;
+    private static final Set<String> LOCALHOST_NAMES = Set.of(
+            "localhost",
+            "LOCALHOST",
+            "127.0.0.1",
+            "::1",
+            "0:0:0:0:0:0:0:0",
+            "0:0:0:0:0:0:0:1"
+    );
+
+    @Getter
+    private final Socket socket;
+
+    // TODO: Decide on a clean implementation to avoid having to call ObjectOutputStream's protected void writeStreamHeader() or give it as custom
+    @Getter
+    private final ObjectOutputStream outputStream;
+
+    // TODO: Decide on a clean implementation to avoid having to call ObjectInputStream's protected void readStreamHeader() or give it as custom
+    private ObjectInputStream inputStream;
+
+
+    public PortableSocket(@NonNull final Socket socket) throws IOException {
+        this.socket = socket;
+        if (socket.isClosed() || socket.getRemoteSocketAddress() == null) {
+            throw new UnsupportedOperationException(
+                    "Invalid way to create a PortableSocket wrapper.",
+                    new IllegalArgumentException("You passed an already closed, "
+                            + "or never previously connected socket reference in the constructor to PortableSocket. "
+                            + "To create a new connection please use the constructor with hostname and port fields.")
+            );
+        }
+
+        setMediumTimeout();
+        this.outputStream = new ObjectOutputStream(socket.getOutputStream());
+    }
+
+    public PortableSocket(@NonNull final String hostname, final int port) throws IOException {
+        socket = new Socket();
+        socket.connect(new InetSocketAddress(hostname, port), THREE_SECONDS_IN_MS);
+        setMediumTimeout();
+        this.outputStream = new ObjectOutputStream(socket.getOutputStream());
+    }
+
+    public static boolean portableIsReachable(@NonNull final String hostname, final int port) throws IOException {
+
+        if (isLocalHostname(hostname)) {
+            return true;
+        }
+
+        try (Socket connectivityTestSocket = new Socket()) {
+
+            InetAddress inetAddr = InetAddress.getByName(hostname);
+            if (inetAddr.isReachable(THREE_SECONDS_IN_MS)) {
+                return true;
+            }
+
+            InetSocketAddress inetSocketAddr = new InetSocketAddress(inetAddr, port);
+            connectivityTestSocket.connect(inetSocketAddr, THREE_SECONDS_IN_MS);
+
+            return true;
+        } catch (IOException e) {
+            throw new IOException("Failed to initiate a connection to: {" + hostname + ":" + port + "} host is unreachable.", e);
+        }
+    }
+
+    public static boolean isLocalHostname(@NonNull final String hostname) throws IOException {
+
+        // Quick and dirty check without having to do any network look-ups
+        if (LOCALHOST_NAMES.contains(hostname)) {
+            return true;
+        }
+
+        InetAddress inetAddress = InetAddress.getByName(hostname);
+        return (inetAddress.isAnyLocalAddress() || inetAddress.isLoopbackAddress() || InetAddress.getLocalHost().equals(inetAddress));
+    }
+
+    public ObjectInputStream initialiseInputStreamWithComponentLoader(@NonNull final URLClassLoader componentLoaderForInputStream) throws IOException {
+        if (inputStream != null) {
+            throw new SocketException("Input Stream already initialised, cannot be re-initialised.");
+        }
+
+        try {
+            inputStream = new ObjectInputStreamWithClassLoader(
+                    this.socket.getInputStream(),
+                    componentLoaderForInputStream
+            );
+        } catch (final IOException e) {
+            throw new IOException("Failed to properly initialise a PortableSocket's input stream", e);
+        }
+
+        return getInputStream();
+    }
+
+    public ObjectInputStream getInputStream() throws IOException {
+        if (inputStream == null) {
+            inputStream = new ObjectInputStream(socket.getInputStream());
+        }
+
+        return inputStream;
+    }
+
+    @Override
+    public void close() {
+        try {
+            if (!socket.isClosed()) {
+                outputStream.flush();
+                socket.close();
+            }
+        } catch (IOException e) {
+            // ignore as going to terminate anyway.
+        }
+    }
+
+    public int setFastTimeout() throws SocketException {
+        return setOneSecondTimeout();
+    }
+
+    public int setMediumTimeout() throws SocketException {
+        return setThreeSecondTimeout();
+    }
+
+    public int setLongTimeout() throws SocketException {
+        return setTenSecondTimeout();
+    }
+
+    public int setOneSecondTimeout() throws SocketException {
+        int previousTimeout = socket.getSoTimeout();
+        socket.setSoTimeout(ONE_SECOND_IN_MS);
+        return previousTimeout;
+    }
+
+    public int setTwoSecondTimeout() throws SocketException {
+        int previousTimeout = socket.getSoTimeout();
+        socket.setSoTimeout(TWO_SECONDS_IN_MS);
+        return previousTimeout;
+    }
+
+    public int setThreeSecondTimeout() throws SocketException {
+        int previousTimeout = socket.getSoTimeout();
+        socket.setSoTimeout(THREE_SECONDS_IN_MS);
+        return previousTimeout;
+    }
+
+    public int setFiveSecondTimeout() throws SocketException {
+        int previousTimeout = socket.getSoTimeout();
+        socket.setSoTimeout(FIVE_SECONDS_IN_MS);
+        return previousTimeout;
+    }
+
+    public int setTenSecondTimeout() throws SocketException {
+        int previousTimeout = socket.getSoTimeout();
+        socket.setSoTimeout(TEN_SECONDS_IN_MS);
+        return previousTimeout;
+    }
+
+    public int setNoTimeout() throws SocketException {
+        int previousTimeout = socket.getSoTimeout();
+        socket.setSoTimeout(NO_TIMEOUT);
+        return previousTimeout;
+    }
+
+    public int setCustomTimeoutInMs(final int timeoutInMilliseconds) throws SocketException {
+        int previousTimeout = socket.getSoTimeout();
+        socket.setSoTimeout(timeoutInMilliseconds);
+        return previousTimeout;
+    }
+}
